a04fc42104a9a702ac1f832a8f8cb9115a7ae5fa
Marshall Jones <vondz1991@gmail.com>
2018-03-04 11:53:15 -0500

    First Commit

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: './src/app.ts',
+    output: {
+        filename: 'app.js',
+        path: __dirname + './dist'
+    },
+    resolve: {
+        extension: ['.ts', '.js']
+    },
+    module: {
+        rules: [
+           { test: /\.ts$/, use:'awesome-typescript-loader' } 
+        ]
+    },
+    devServer: {
+        port: 3000,
+    }
+};
